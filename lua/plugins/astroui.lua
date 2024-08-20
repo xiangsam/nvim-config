bd30b848fc0f2510f84a69ab7fea5fd59784b2ce
xiangsam <huang.xiang@buaa.edu.cn>
2024-08-20 14:36:52 +0800

    modify ui

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -3,35 +3,33 @@
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
 
+local light_theme, dark_theme = "astrolight", "catppuccin"
 ---@type LazySpec
 return {
-  "AstroNvim/astroui",
-  ---@type AstroUIOpts
-  opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      -- change colorscheme
+      colorscheme = dark_theme,
+      -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+      -- Icons can be configured throughout the interface
+      icons = require "plugins.ui.icons",
+      text_icons = require "plugins.ui.text_icons",
     },
-    -- Icons can be configured throughout the interface
-    icons = {
-      -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
+  },
+  -- Theming
+  { "Shatur/neovim-ayu", lazy = true },
+  { "mcchrish/zenbones.nvim", lazy = true, dependencies = { "rktjmp/lush.nvim" } },
+  { "navarasu/onedark.nvim", lazy = true },
+  { "nyoom-engineering/oxocarbon.nvim", lazy = true },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    opts = {
+      background = { light = "latte", dark = "mocha" },
     },
   },
 }
---
+++ lua/plugins/ui/icons.lua
@@ -0,0 +1,20 @@
+---@type table<string, string>
+return {
+  -- configure the loading of the lsp in the status line
+  LSPLoading1 = "⠋",
+  LSPLoading2 = "⠙",
+  LSPLoading3 = "⠹",
+  LSPLoading4 = "⠸",
+  LSPLoading5 = "⠼",
+  LSPLoading6 = "⠴",
+  LSPLoading7 = "⠦",
+  LSPLoading8 = "⠧",
+  LSPLoading9 = "⠇",
+  LSPLoading10 = "⠏",
+  BufferClose = "󰅖",
+  FileModified = "*",
+  FoldClosed = ">",
+  FoldOpened = "v",
+  Selected = "*",
+  TabClose = "󰅖",
+}
